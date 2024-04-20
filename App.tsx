9220c5abb8b8e9d4cb77892eccd382294aa8d362
HasnainMalik <apple@apples-MBP.bbrouter>
2024-04-21 01:24:34 +0500

    react persist issue

--- App.tsx
+++ App.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 import StackNav from "./src/navigations/stackNav";
 import { Provider } from "react-redux";
-import { Store } from "./src/redux/store";
-import { Persistor } from "redux-persist";
-import persistStore from "redux-persist/es/persistStore";
+import { Store,persistor } from "./src/redux/store";
 import { PersistGate } from "redux-persist/integration/react";
-PersistGate
-let persistor = persistStore(Store);
+import persistStore from "redux-persist/lib/persistStore";
+
+// let persistor = persistStore(Store);
 const App=()=>{
   return(
     <Provider store={Store}>
-      <PersistGate persistor={persistor}>
+      <PersistGate loading={null} persistor={persistor}>
     <StackNav />
     </PersistGate>
     </Provider>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,23 +1,24 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from 'redux-persist'; // Correct import
+import storage from "redux-persist/lib/storage";
 import UniReducer from './wishlist/Uni_wishlist'
 import jobReducer from './wishlist/Job_slice'
 import Scholarship_Reducer from './wishlist/Scholarship_slice';
-import persistReducer from "redux-persist/es/persistReducer";
-import storage from "redux-persist/lib/storage";
 
 const persistConfig = {
     key: 'root',
     storage,
-  }
+}
 
-let rootReducer=combineReducers({
-    uni:UniReducer,
-        job:jobReducer,
-        scholarship: Scholarship_Reducer,
-})
+const rootReducer = combineReducers({
+    uni: UniReducer,
+    job: jobReducer,
+    scholarship: Scholarship_Reducer,
+});
 
-const persistedReducer = persistReducer(persistConfig, rootReducer)
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const Store=configureStore({
-   reducer: persistedReducer
-}) 
+export const Store = configureStore({
+    reducer: persistedReducer
+});
+export const persistor=persistStore(Store);
